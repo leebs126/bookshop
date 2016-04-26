0bd43eaa87e015c26a33816543227b8461b8e8cc
leebs126 <leebs126@gmail.com>
2016-04-26 14:33:49 +0900

    55

---
+++ Shoping5/WebContent/jsp/admin/common/a_footer.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<FOOTER>
+<UL>
+	<LI><A href="#">회사소개</A></LI>
+	<LI><A href="#">이용약관</A></LI>
+	<LI><A href="#">개인정보취급방침</A></LI>
+	<LI><A href="#">광고/이벤트
+			문의</A></LI>
+	<LI><A href="#">고객만족센터</A></LI>
+	<LI class="no_line"><A
+		href="#">찾아오시는길</A></LI>
+</UL>
+<DIV class="clear"></DIV>
+<A href=""><IMG width="147"
+	height="62" alt="SENOP" src="<%=request.getContextPath()%>/image/foot_img.gif"></A>
+<DIV id="foot_note">
+	고객상담 전화주문:1577-11111121 사업자등록번호 :106-11-123456 대표이사: 양용석 통신판매업 서울 제
+	000000호<BR> 개인정보관리책임자:ugpapa 분쟁조정기관표시 : 소비자보호원, 전자거래분쟁중재위원회<BR>
+	이메일주소무단수집거부 help@senopbiz.com<BR> Copyright (c) 20111 Senop Corp.
+	All right Reserved.
+</DIV>
+</DIV>
+</FOOTER>
+<DIV id="sticky">
+	<UL>
+		<LI><A href="#"><IMG
+				width="24" height="24" src="<%=request.getContextPath()%>/image/facebook_icon.png">
+				페이스북</A></LI>
+		<LI><A href="#"><IMG
+				width="24" height="24" src="<%=request.getContextPath()%>/image/twitter_icon.png">
+				트위터</A></LI>
+		<LI><A href="#"><IMG
+				width="24" height="24" src="<%=request.getContextPath()%>/image/rss_icon.png">
+				RSS 피드</A></LI>
+	</UL>
+	<DIV class="recent">
+		<H3>최근 본 상품</H3>
+		<UL>
+			<LI><IMG width="75" height="95" src="<%=request.getContextPath()%>/image/html5_icon.jpg"></LI>
+			<LI><IMG width="75" height="95" src="<%=request.getContextPath()%>/image/jquery_icon.jpg"></LI>
+		</UL>
+	</DIV>
+</DIV>
+</DIV>
+
+</html>
---
+++ Shoping5/src/com/shop/ex1/common/file/DownLoadServlet.java
@@ -0,0 +1,54 @@
+package com.shop.ex1.common.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/download.do")
+public class DownLoadServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException,IOException{
+			doProcess(request, response);
+		}
+		
+		protected void doPost(HttpServletRequest request,HttpServletResponse response)
+		  throws ServletException, IOException{
+			doProcess(request, response);
+		}
+		  
+		
+		protected void doProcess(HttpServletRequest request,HttpServletResponse response)
+		  throws ServletException, IOException{
+			request.setCharacterEncoding("EUC-KR");
+			response.setContentType("text/html; charset=EUC-KR");
+			
+			//자바 I/O를 이용해서 파일을 전송한다.
+			String file = (String)request.getParameter("fileName");
+			System.out.println("file="+file);
+			OutputStream out = response.getOutputStream();
+			String fileName="c:\\repo\\"+file;
+			File f=new File(fileName);
+			//response.setHeader("Cache-Control", "no-cache");
+			//response.addHeader("Content-disposition", "attachment; filename=" + fileName);
+			FileInputStream in=new FileInputStream(f); //파일을 읽어온다.
+			
+			byte[] buffer=new byte[1024*8];
+			while(true){
+				int count=in.read(buffer); //버퍼에 읽어들인 문자개수
+				if(count==-1)  //버퍼의 마지막에 도달했는지 체크
+					break;
+				out.write(buffer,0,count);
+			}
+			
+			in.close();
+			out.close();
+	    }
+
+}
